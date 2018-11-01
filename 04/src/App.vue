3e026ebeec3b53d5e1920daaceb500e84c74e5e5
hanguksaram <artur.pozhidaev@gmail.com>
2018-11-01 19:11:25 +0700

    dynamically passed components, activated deactivated lifehooks

---
+++ 04/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+                <!-- way to pass components dinamically -->
+                <keep-alive>
+                    <component :is="selectedComponent">
+                    default content
+                    </component>
+                </keep-alive>
+                              <!-- <app-quote>
+                    <h2 slot="title">{{quoteTitle}}</h2>
+                    <p>A wonedrfull quote</p>
+               </app-quote> -->
+               </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Author from './components/Author.vue'
+    import New from './components/New.vue'
+    import Quote from './components/Quote.vue'
+    export default {
+        data() {
+            return {
+                quoteTitle: 'The quote',
+                selectedComponent: 'appQuote'
+            }
+        },
+        components: {
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ 04/src/components/Quote.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div class="title">
+            <slot name="title"></slot>
+            <span style="color: #ccc"><slot name="subtitle">The subtitle</slot></span>
+        </div>
+        <hr>
+        <div>
+            <slot></slot>
+        </div>
+     <!-- named and unnamed slots works together perfectly -->
+    </div>
+</template>
+<script>
+export default {
+   
+}
+</script>
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+    /*child component styling apply to data passed from parent via <slot></slot>*/
+    h2 {
+        color: red;
+    }
+    .title {
+        font-style: italic;
+    }
+</style>

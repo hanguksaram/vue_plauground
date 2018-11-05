2e6337d4dd342dc96b28afa875a2e69f32b810c6
hanguksaram <artur.pozhidaev@gmail.com>
2018-11-05 20:28:48 +0700

    adding routes

---
+++ 12/src/components/user/UserDetail.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h3>Some user details</h3>
+        <p>User loaded has id: {{ $route.params.id}}</p>
+        <router-link 
+            tag="button" 
+            :to="{ name: 'userEdit', params: { id: $route.params.id}, query: { locale: 'en', q: 100 }}"
+            class="btn btn-primary">Edit user</router-link>
+    </div>
+</template>
